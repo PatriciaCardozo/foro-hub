648a6ef16966b75a5070c7ce4e69a5919fc86c13
Patricia <patriciabader39@gmail.com>
2026-03-10 13:02:02 +0100

    Add Topico.java entity with basic fields

---
+++ src/main/java/Topico.java
@@ -0,0 +1,9 @@
+public class Topico {
+    private Long id;
+    private String titulo;
+    private String mensaje;
+    private String fechaCreacion;
+    private String status;
+    private String autor;
+    private String curso;
+}
